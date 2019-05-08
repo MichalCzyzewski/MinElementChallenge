1b6001e75f388a35988b86d3dd80c90c5b78a5c2
Michal Czyzewski <czy.mich@gmail.com>
2019-05-08 23:28:19 +0200

    add readInteger method and findMin method

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -1,8 +1,44 @@
 package pl.mczyzewski;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+
+
+    }
+
+
+    public static int[] readIntegers()
+    {
+        System.out.print("How many Integers you want to enter ? :");
+        int count = sc.nextInt();
+        int[] arrayEnter = new int[count];
+        System.out.println("Pleas enter " + count + " Integers numbers :");
+        for (int x = 0; x < arrayEnter.length; x++)
+        {
+            arrayEnter[x] = sc.nextInt();
+        }
+
+            return arrayEnter;
+    }
+
+    public static int findMin(int[] array)
+    {
+        int minNumber = Integer.MAX_VALUE;
+
+        for (int x = 0; x< array.length;x++)
+        {
+            if (array[x] < minNumber )
+            {
+                minNumber = array[x];
+            }
+        }
+        return minNumber;
     }
 }
